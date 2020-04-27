e5b538a6b708fe16bfca1d702364d0c212b38dcf
H3l1c35 <64040203+H3l1c35@users.noreply.github.com>
2020-04-27 14:41:42 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-$(function() {
-    $("#add").on("click", function() {
-        var val = $("input").val();
+// $(".add").on("click", function() {
+//     var val = $("input").val();
+//     if(val !== '') {
+//         var elem = $("<li></li>").text(val);
+//         $(elem).append("<button class='rem'>X</button>");
+//         $("#mylist").append(elem);
+//         $("input").val("");
+//         $(".rem").on("click", function() {
+//             $(this).parent().remove();
+//         });
+//     }
+// });
+   $(".add").on("click", function() {
+        var val = $(this).parent().children( "input").val();
         if(val !== '') {
             var elem = $("<li></li>").text(val);
             $(elem).append("<button class='rem'>X</button>");
-            $("#mylist").append(elem);
+            $(this).parent().children( "#mylist").append(elem);
             $("input").val("");
             $(".rem").on("click", function() {
                 $(this).parent().remove();
             });
         }
     });
-});
