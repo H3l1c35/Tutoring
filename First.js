dea9149fffc2e16d59db7ee5199cc2483f017c12
H3l1c35 <64040203+H3l1c35@users.noreply.github.com>
2020-04-27 15:06:58 -0400

    Update First.js

--- First.js
+++ First.js
@@ -1,26 +1,14 @@
-  
-// $(".add").on("click", function() {
-//     var val = $("input").val();
-//     if(val !== '') {
-//         var elem = $("<li></li>").text(val);
-//         $(elem).append("<button class='rem'>X</button>");
-//         $("#mylist").append(elem);
-//         $("input").val("");
-//         $(".rem").on("click", function() {
-//             $(this).parent().remove();
-//         });
-//     }
-// });
+
 alert("Wasup");
-   $(".add").on("click", function() {
-        var val = $(this).parent().children( "input").val();
-        if(val !== '') {
-            var elem = $("<li></li>").text(val);
-            $(elem).append("<button class='rem'>X</button>");
-            $(this).parent().children( "#mylist").append(elem);
-            $("input").val("");
-            $(".rem").on("click", function() {
-                $(this).parent().remove();
-            });
-        }
-    });
+$(".add").on("click", function() {
+  var val = $(this).parent().children( "input").val();
+  if(val !== '') {
+    var elem = $("<li></li>").text(val);
+      $(elem).append("<button class='rem'>X</button>");
+      $(this).parent().children( "#mylist").append(elem);
+      $("input").val("");
+      $(".rem").on("click", function() {
+         $(this).parent().remove();
+      });
+  }
+});
