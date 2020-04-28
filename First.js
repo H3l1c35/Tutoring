8c19b94b4dd7ecee474a76706331b0fcc234f0ca
H3l1c35 <64040203+H3l1c35@users.noreply.github.com>
2020-04-28 10:49:17 -0400

    Update First.js

--- First.js
+++ First.js
@@ -1,26 +1,4 @@
-// $(".add").on("click", function() {
-//     var val = $("input").val();
-//     if(val !== '') {
-//         var elem = $("<li></li>").text(val);
-//         $(elem).append("<button class='rem'>X</button>");
-//         $("#mylist").append(elem);
-//         $("input").val("");
-//         $(".rem").on("click", function() {
-//             $(this).parent().remove();
-//         });
-//     }
-// });
-
-$(document).ready(function(){
-   alert("ary");
-$("h1").on("click", function(){
-    alert("hilkosl");
-   });
-   
-    $(".section").on("click", "addB", function() {
-       alert("hi")
-   
-   /**
+ $("button").on("click", function() {
         var val = $(this).parent().children( "input").val();
         if(val !== '') {
             var elem = $("<li></li>").text(val);
@@ -30,17 +8,5 @@ $("h1").on("click", function(){
             $(".rem").on("click", function() {
                 $(this).parent().remove();
             });
-        }*/
+        }
     });
-});
-
-  /**
-   $("label").on("click", function(){
-    alert("hi");
-   });
-$(".addB").on("click", function(){
-   alert("bob");
-  });
-
-  
-*/
