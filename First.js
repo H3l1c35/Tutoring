5a5b37ab3350cf807044d5d6575da4bd0405b60f
H3l1c35 <64040203+H3l1c35@users.noreply.github.com>
2020-04-27 23:30:50 -0400

    Update First.js

--- First.js
+++ First.js
@@ -11,11 +11,11 @@
 //     }
 // });
 alert("hi");
-
+/**
 $(".addB").on("click", function(){
    alert("bob");
   )};
-   /**
+   
    $(".add").on("click", function() {
         var val = $(this).parent().children( "input").val();
         if(val !== '') {
