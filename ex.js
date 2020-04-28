6183d0feceb1ed35937a8f8d671b58da2e6f1e0b
H3l1c35 <64040203+H3l1c35@users.noreply.github.com>
2020-04-27 23:40:41 -0400

    Create ex.js

---
+++ ex.js
@@ -0,0 +1,20 @@
+
+$('#contact-form').submit(function(e) {
+  var name = document.getElementById('inputName');
+   email = document.getElementById('inputEmial');
+   message = document.getElementById('inputMessage');
+  
+  if (!name.value || !email.value || !message.value) {
+    alertify.error('Please check your entries')
+  } else {
+    $.ajax({
+      url: "http://formspree.io/bi22k@mtholyoke.edu",
+      method:"POST",
+      data: $(this).serialize(),
+      dataType: "json"
+    });
+    e.preventDefault()
+    $(this).get(0).reset()
+    altertify.success('Message sent')
+  }
+})
